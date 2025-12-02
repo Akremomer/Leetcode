34e8ecf915b3bd4db2a576c3063e19d0ae724da2
Akrem_Omer <133592980+Akremomer@users.noreply.github.com>
2025-12-02 07:10:43 +0300

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def twoSum(self,nums, target):
         dic={}
-        for i in range(len(nums)): 
+        for i in range(len(nums)):  
           diff=target-nums[i]
          if diff in dic:
             return [dic[diff],i] 
